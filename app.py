9998e3f67bec01c38468116d09327b1b75d1e9b3
coldarra1 <594933436@qq.com>
2023-02-23 16:36:16 +0800

    fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import configs as configs
+import config as config
 from flask import Flask
 from gevent import pywsgi
 
@@ -6,9 +6,15 @@ from exts import db
 
 app = Flask(__name__)
 # 加载配置文件
-app.config.from_object(configs)
+app.config.from_object(config)
 # db绑定app
 db.init_app(app)
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return "hello!"
+
 
 if __name__ == '__main__':
     server = pywsgi.WSGIServer(('127.0.0.1', 5000), app)
